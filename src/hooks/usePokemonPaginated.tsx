0fe5dc7451f553fc08a150d5842ad0dd60f82c4d
Juan Jose Mayorga <devjuanjose@gmail.com>
2022-10-17 07:01:21 -0500

    to start the usePokemonPaginated.tsx hook

---
+++ src/hooks/usePokemonPaginated.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from 'react';
+import { pokemonApi } from '../api/pokemonApi';
+
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
+
+export const usePokemonPaginated = () => {
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
+  const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
+
+  const loadPokemons = async () => {
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList(resp.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.forEach(pokemon => console.log(pokemon));
+  };
+
+  useEffect(() => {
+    loadPokemons();
+  }, []);
+
+  return {
+    simplePokemonList,
+  };
+};
