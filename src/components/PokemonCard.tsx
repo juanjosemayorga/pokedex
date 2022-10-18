78d9510425c81b7c18286c0d054737ecdd98303d
Juan Jose Mayorga <devjuanjose@gmail.com>
2022-10-18 08:57:30 -0500

    create PokemonCard

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { SimplePokemon } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
+
+const windowWidth = Dimensions.get('window').width;
+
+interface Props {
+  pokemon: SimplePokemon
+}
+
+export const PokemonCard = ({ pokemon }: Props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.9}>
+      <View style={{
+        ...styles.cardContainer,
+        width: windowWidth * 0.4,
+      }}>
+        <View>
+          <Text style={styles.name}>
+            {pokemon.name + '\n'} #{pokemon.id}
+          </Text>
+        </View>
+
+        <View style={styles.pokebolaContainer}>
+          <Image
+            source={ require('../assets/pokebola-blanca.png') }
+            style={styles.pokebola}
+          />
+        </View>
+
+        <FadeInImage
+          uri={pokemon.picture}
+          style={styles.pokemonImage}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    marginHorizontal: 10,
+    backgroundColor: 'tomato',
+    height: 120,
+    width: 160,
+    marginBottom: 25,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  name: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    top: 10,
+    left: 10,
+  },
+  pokebola: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    right: -25,
+    bottom: -25,
+  },
+  pokemonImage: {
+    width: 120,
+    height: 120,
+    position: 'absolute',
+    right: -8,
+    bottom: -5,
+  },
+  pokebolaContainer: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    overflow: 'hidden',
+    opacity: 0.5,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,27 +1,51 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../components/PokemonCard';
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 import { globalStyles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
-  usePokemonPaginated();
+  const { simplePokemonList, isLoading, loadPokemons } = usePokemonPaginated();
 
   return (
     <>
       <Image
-        source={ require('../assets/pokebola.png') }
+        source={require('../assets/pokebola.png')}
         style={globalStyles.pokebolaBG}
       />
-      <Text
-        style={{
-          ...globalStyles.title,
-          ...globalStyles.globalMargin,
-          top: top + 20,
-        }}>
-        Pokedex
-      </Text>
+      <View
+        style={{ alignItems: 'center' }}
+      >
+        <FlatList
+          data={simplePokemonList}
+          keyExtractor={pokemon => pokemon.id}
+          showsVerticalScrollIndicator={false}
+          // to show in two columns
+          numColumns={2}
+          // header
+          ListHeaderComponent={
+            <Text
+              style={{
+                ...globalStyles.title,
+                ...globalStyles.globalMargin,
+                top: top + 20,
+                marginBottom: top + 20,
+                paddingBottom: 10,
+              }}>
+              Pokedex
+            </Text>
+          }
+          renderItem={({item}) => <PokemonCard pokemon={item} />}
+          // Infinite scroll
+          onEndReached={loadPokemons}
+          onEndReachedThreshold={0.4}
+          ListFooterComponent={
+            <ActivityIndicator style={{height: 100}} size={20} color="grey" />
+          }
+        />
+      </View>
     </>
   );
 };
